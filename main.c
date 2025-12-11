bb6164ee86635e045e7b9f25212f048ff5ddfebd
kevin kiplangat <kiplangatkevin335@gmail.com>
2025-12-11 13:59:57 +0300

    variables

--- main.c
+++ main.c
@@ -3,7 +3,13 @@
 
 int main()
 {
-    printf("Hello, world\n");
+    char characterName[] = "John";
+    int characterAge = 35;
+    
+    printf("There once was a man named %s\n",characterName );
+    printf("he was %d years old. \n", characterAge );
+    printf("he really liked the name %s \n", characterName);
+    printf("but he did not like being %d.\n", characterAge);
 
     return 0;
 
