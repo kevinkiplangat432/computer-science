02351c0091df71ccb389c9ba151a0805582e7d7f
kevin kiplangat <kiplangatkevin335@gmail.com>
2025-12-11 14:10:49 +0300

    printf and data-types

---
+++ data-types.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age =40;
+    double gpa =3.0; // decimal number
+    char grade = "A"; // one single character
+    char phrase[] = "name"; //store multiple charaters by using and arr
+
+
+    return 0;
+
+}
